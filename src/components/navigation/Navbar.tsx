f759b9acedff5bbdc3501d2b85a87acaacd363a9
arsenharutjunjan <nuvant.software@gmail.com>
2025-12-19 17:42:37 +0100

    Update Navbar.tsx

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -248,7 +248,7 @@ const Navbar: React.FC = () => {
       {/* Mobile sidebar */}
       <div
         className={`
-          fixed top-0 left-0 h-full w-64 bg-white shadow-lg transform
+          fixed top-0 left-0 h-full w-64 !bg-white shadow-lg transform
           transition-transform duration-300 ease-in-out hidden max-[1366px]:block z-[110]
           ${isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full'}
         `}
